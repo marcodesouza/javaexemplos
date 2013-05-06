570d9f34494ec8f387c1918413bb496b97543fd4
marcodesouza <marco@ifc-sombrio.edu.br>
2013-05-06 09:57:12 -0300

    Update MaiordeTres.java

--- src/MaiordeTres.java
+++ src/MaiordeTres.java
@@ -1,20 +1,6 @@
-
 public class MaiordeTres {
     public static void main(String[] args) {
         
-        int a=3, b=5, c=2, maior=0, menor;
-        
-        if (a>b & a>c) {
-            maior=a;
-            menor=c;
-        } else if (b>a && b>c)  {
-            maior=b;
-            menor=c;
-        } else if (c>a & c>b){
-            maior=c;
-            menor=b;
-        }
-        
-        System.out.println("Maior número = "+maior);
+
     }
 }
