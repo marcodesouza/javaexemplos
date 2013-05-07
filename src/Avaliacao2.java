c5c7b27075e1e80cb08ac66c44afd0d69a420ebd
marcodesouza <marco@ifc-sombrio.edu.br>
2013-05-07 08:53:38 -0300

    Update Avaliacao2.java

--- src/Avaliacao2.java
+++ src/Avaliacao2.java
@@ -2,17 +2,7 @@ import java.util.Scanner;
 
 public class Avaliacao2 {
 	public static void main(String args[]) {
-	       
-	       double   x=10;
-	       double   soma=2;
-               boolean  simNao=true;
-               char     tecla='c';
-              
-	       x=soma*soma*soma;
-               
-               if (simNao) {
-                 System.out.println("resultado: "+ x); 
-            }
+
 
     }
 }
